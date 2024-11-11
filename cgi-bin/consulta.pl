83549f0d163802c4aad2caa90cb3b76aead51420
leonhatches <jhatches@unsa.edu.pe>
2024-11-11 18:27:17 -0500

    Version con decodificacion correcta | Error de mala lectura en licencia

--- cgi-bin/consulta.pl
+++ cgi-bin/consulta.pl
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 use CGI;
 use utf8;
+use Encode;
 
 my $cgi = CGI->new;
 
@@ -13,6 +14,7 @@ $cgi->charset('UTF-8');
 
 my $opcion = $cgi->param("opcion");
 my $entrada = $cgi->param("entrada");
+$entrada = decode('UTF-8', $entrada);
 
 print<<HTML;
 <!DOCTYPE html>
@@ -44,49 +46,51 @@ HTML
 my $i = 0;
 if ( !($opcion eq "") && !($entrada eq "") )
 {
-	print"<div class=\"cajaResultado\">";
-	print"<h1>RESULTADOS</h1>";
-	print"</div>";
-	print"<h2 class=\"caja\">Búsqueda: $entrada</h2>";
+	print "<div class=\"cajaResultado\">\n";
+	print "<h1>RESULTADOS</h1>\n";
+	print "</div>\n";
+	print "<h2 class=\"caja\">Búsqueda: $entrada</h2>\n";
 
-	open(IN,"./universidades.txt") or die print "<h1>ERROR: open file</h1>\n";
+	open my $archivo, "<:encoding(UTF-8)", "./universidades.txt" or die print"<h2 class=\"caja\">Error abriendo el archivo</h2>";
 
-	while (my $linea = <IN>)
+	while (my $linea = <$archivo>)
 	{
 		my %dict = match($linea);
 		my $valor = $dict{$opcion};
 
-		if(defined($valor) && $valor =~ /.*$entrada.*/ && ($i % 2 == 0))
+		if(defined($valor) && $valor =~ /.*$entrada.*/i && ($i % 2 == 0))
 		{
 	  		print "<p class=\"cajaBlanca\">$linea</p>\n";
-		    $i = $i + 1;
+		    $i++;
 		    next;
 		}
-		elsif (defined($valor) && $valor =~ /.*$entrada.*/ && ($i % 2 == 1))
+		elsif (defined($valor) && $valor =~ /.*$entrada.*/i && ($i % 2 == 1))
 		{
 			print "<p class=\"cajaGris\">$linea</p>\n";
-		    $i = $i + 1;
+		    $i++;
 		    next;
 		}
     }
+    close $archivo;
 }
 else {
 	print"<div class=\"cajaResultado\">";
 	print"<h1>INGRESE DATOS ...</h1>";
 	print"</div>";
 }
+
+print"<h2 class=\"caja\">Cantidad de Resultados: $i</h2>";
 print "</body></html>";
 
-
-
-sub match {
+sub match
+{
 	my %dict = ();
 	my $linea = $_[0];
 
 	if ( $linea =~ /\|(.*?)\|(?:.*?\|){4}(.*?)\|(?:.*?\|){5}(.*?)\|(?:.*?\|){5}(.*?)\|/ )
 	{
-		$dict{"nombre"} = $1;
-		$dict{"licencia"} = $2;
+		$dict{"nombre"}		  = $1;
+		$dict{"licencia"}	  = $2;
 		$dict{"departamento"} = $3;
 		$dict{"denominacion"} = $4;
 	}
